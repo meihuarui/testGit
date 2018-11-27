f2fa782086da1f7814c7c85c5347d44f98670ca8
meihuarui <43111815+meihuarui@users.noreply.github.com>
2018-11-27 17:39:23 +0800

    Update TestGit.java

--- src/cc/rengu/TestGit.java
+++ src/cc/rengu/TestGit.java
@@ -5,5 +5,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("111111");
         System.out.println("22222");
+        System.out.println("3333333");
     }
 }
