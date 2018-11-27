8474e3cf748e00242868153bbb60c58a9bfb6bb7
wsh11730 <chaoyang.wei@baozun.com>
2018-11-27 19:45:31 +0800

    测试创建分支

--- gitdemo/src/cc/rengu/GitDemo.java
+++ gitdemo/src/cc/rengu/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("gitDemo01");
          System.out.println("gitDemo01");
+         System.out.println("gitDemo01");
     }
 }
