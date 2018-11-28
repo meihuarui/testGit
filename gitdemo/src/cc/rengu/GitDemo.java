60c4cb29d63c9b1d814091f9fe66a290e1aeb4b8
wsh11730 <chaoyang.wei@baozun.com>
2018-11-28 10:30:16 +0800

    回滚4

--- gitdemo/src/cc/rengu/GitDemo.java
+++ gitdemo/src/cc/rengu/GitDemo.java
@@ -3,8 +3,8 @@ package cc.rengu;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("gitDemo01");
-         System.out.println("gitDemo01");
-         System.out.println("gitDemo01");
+        System.out.println("gitDemo01");
+        System.out.println("gitDemo01");
         System.out.println("git测试回滚");
         System.out.println("git测试ignore");
         System.out.println("git测试创建1128分支");
