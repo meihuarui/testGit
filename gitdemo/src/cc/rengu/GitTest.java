2096332fa74a2c24ac0ba008140243dd81f3f398
wsh11730 <chaoyang.wei@baozun.com>
2018-11-29 14:53:59 +0800

    测试huigun

--- gitdemo/src/cc/rengu/GitTest.java
+++ gitdemo/src/cc/rengu/GitTest.java
@@ -2,10 +2,10 @@ package cc.rengu;
 
 public class GitTest {
     public static void main(String[] args){
-
         System.out.println("test live templates");
         for (int i = 0; i < 10; i++) {
             System.out.println("111");
+            System.out.println();
         }
     }
     
